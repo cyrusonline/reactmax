a4902f2d65e85223bb25f6117b1b62dddab99fe5
cyrus <online_mad@yahoo.com.hk>
2018-07-30 21:55:34 +0800

    modifiy at home

--- src/App.js
+++ src/App.js
@@ -2,13 +2,33 @@ import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person'
 class App extends Component {
+  state = {
+    persons:[
+      {name:'Max',age:28},
+      {name:'Manu',age:29},
+      {name:'Stephanie',age:26},
+    ]
+  }
+
+  switchNameHandler = (newName) =>{
+    this.setState({
+    persons:[
+      {name:newName,age:28},
+      {name:'Manu',age:29},
+      {name:'Stephanie',age:33},
+    ]
+    
+  })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi i am an app</h1>
-        <Person name="Max" age="28"/>
-        <Person name="Cyrus" age="8">My hobby is racing</Person>
-        <Person name="Peter" age="18"/>
+        <button onClick={this.switchNameHandler.bind(this,'MongoDB')}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} click={this.switchNameHandler.bind(this,'Gogogo')}>My hobby is racing</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
     
       </div>
     
